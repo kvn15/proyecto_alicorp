a2a0fbba47b9580604d848b27606ed9086052a5a
Polanca <paul_luis@yahoo.com>
2025-01-04 08:26:35 -0500

    mejoras front3

--- resources/views/juegos.blade.php
+++ resources/views/juegos.blade.php
@@ -1,9 +1,8 @@
-
 @include('cabecera/header-2')
 
 @php
 
-    //$dominio = App\Models\Project::where('dominio', $hub)->where('status', 1)->where('game_id', 1)->first();   
+    //$dominio = App\Models\Project::where('dominio', $hub)->where('status', 1)->where('game_id', 1)->first();
     //$Allcards = App\Models\AdminPanel\PromocionesCard::all();
 @endphp
 
@@ -47,77 +46,80 @@
 </section> --}}
 
 <section class="juego">
-<div class="container">
-    <div class="row">
-        <div class="col-md-6 titulo-2">
-            <h2>Momentos divertidos</h2>
-            <h3>Despierta <br> La diversión!</h3>
-            <p>Juéganos con tus marcas favoritas y gana <br> premios Increíbles.</p>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 titulo-2">
+                <h2>Momentos divertidos</h2>
+                <h3>Despierta <br> La diversión!</h3>
+                <p>Juéganos con tus marcas favoritas y gana <br> premios Increíbles.</p>
+            </div>
+            <div class="col-md-6 img-juego-c">
+                <img src="{{ asset('img/juegos-img.png') }}" alt="" class="img-fluid">
+            </div>
         </div>
-        <div class="col-md-6 img-juego-c">
-            <img src="{{asset('img/juegos-img.png')}}" alt="" class="img-fluid">
+        <div class="row">
+
         </div>
     </div>
-    <div class="row">
-        
-    </div>
-</div>
 </section>
 <section class="juegos">
     <div class="container">
         <div class="row">
-            <div class="card sec-juegos me-4">
-                <div class="img-transicion">
-                    <img src="{{ asset('img/galleta.png') }}" alt="" class="galletex">
-                    <img src="{{ asset('img/indice.png') }}" alt="" class="back position-absolute">
+            <div class="col-md-12 d-flex">
+                <div class="card sec-juegos me-4 card-1">
+                    <div class="img-transicion">
+                        <img src="{{ asset('img/galleta.png') }}" alt="" class="galletex">
+                        <img src="{{ asset('img/indice.png') }}" alt="" class="back position-absolute">
+                    </div>
+                    <div class="card-content">
+                        <h1 class="card-title">¡Casino te trae <br> el Raspa y Gana!</h1>
+                        <p class="card-subtitle">Este es un subtítulo descriptivo.</p>
+                    </div>
+                    <div class="card-footer">
+                        <button class="btn btn-danger">JUEGA YA</button>
+                    </div>
                 </div>
-                <div class="card-content">
-                  <h1 class="card-title">¡Casino te trae <br> el Raspa y Gana!</h1>
-                  <p class="card-subtitle">Este es un subtítulo descriptivo.</p>
+                <div class="card sec-juegos me-4 card-2">
+                    <div class="img-transicion">
+                        <img src="{{ asset('img/carta.png') }}" alt="" class="carta">
+                        <img src="{{ asset('img/carta.png') }}" alt="" class="carta2">
+                        <img src="{{ asset('img/carta.png') }}" alt="" class="carta3">
+                    </div>
+                    <div class="card-content">
+                        <h1 class="card-title">Encuentra tu <br> Pasta ideal <br> con Don Vittorio </h1>
+                        <p class="card-subtitle">Gana premios para lucirte <br> en la cocina</p>
+                    </div>
+                    <div class="card-footer">
+                        <button class="btn btn-danger">JUEGA YA</button>
+                    </div>
                 </div>
-                <div class="card-footer">
-                  <button class="btn btn-danger">JUEGA YA</button>
+                <div class="card sec-juegos me-4 card-3">
+                    <div class="img-transicion">
+                        <img src="{{ asset('img/ruleta2.png') }}" alt="" class="ruleta">
+                    </div>
+                    <div class="card-content">
+                        <h1 class="card-title">Gira tu melena <br> con Amaras</h1>
+                        <p class="card-subtitle">Gana premios para que <br> sigas cuidadno de ti.</p>
+                    </div>
+                    <div class="card-footer">
+                        <button class="btn btn-danger">JUEGA YA</button>
+                    </div>
                 </div>
-            </div>
-            <div class="card sec-juegos me-4">
-                <div class="img-transicion">
-                    <img src="{{ asset('img/galleta.png') }}" alt="" class="galletex">
-                    <img src="{{ asset('img/indice.png') }}" alt="" class="back position-absolute">
+                <div class="card sec-juegos me-4">
+                    <div class="img-transicion">
+                        <img src="{{ asset('img/galleta.png') }}" alt="" class="galletex">
+                        <img src="{{ asset('img/indice.png') }}" alt="" class="back position-absolute">
+                    </div>
+                    <div class="card-content">
+                        <h1 class="card-title">¡Casino te trae <br> el Raspa y Gana!</h1>
+                        <p class="card-subtitle">Este es un subtítulo descriptivo.</p>
+                    </div>
+                    <div class="card-footer">
+                        <button class="btn btn-danger">JUEGA YA</button>
+                    </div>
                 </div>
-                <div class="card-content">
-                  <h1 class="card-title">¡Casino te trae <br> el Raspa y Gana!</h1>
-                  <p class="card-subtitle">Este es un subtítulo descriptivo.</p>
-                </div>
-                <div class="card-footer">
-                  <button class="btn btn-danger">JUEGA YA</button>
-                </div>
-            </div>
-            <div class="card sec-juegos me-4 card-3">
-                <div class="img-transicion">
-                    <img src="{{ asset('img/ruleta2.png') }}" alt="" class="ruleta">
-                </div>
-                <div class="card-content">
-                  <h1 class="card-title">Gira tu melena <br> con Amaras</h1>
-                  <p class="card-subtitle">Gana premios para que <br> sigas cuidadno de ti.</p>
-                </div>
-                <div class="card-footer">
-                  <button class="btn btn-danger">JUEGA YA</button>
-                </div>
-            </div>
-            <div class="card sec-juegos me-4">
-                <div class="img-transicion">
-                    <img src="{{ asset('img/galleta.png') }}" alt="" class="galletex">
-                    <img src="{{ asset('img/indice.png') }}" alt="" class="back position-absolute">
-                </div>
-                <div class="card-content">
-                  <h1 class="card-title">¡Casino te trae <br> el Raspa y Gana!</h1>
-                  <p class="card-subtitle">Este es un subtítulo descriptivo.</p>
-                </div>
-                <div class="card-footer">
-                  <button class="btn btn-danger">JUEGA YA</button>
-                </div>
-            </div>  
+            </div>           
         </div>
     </div>
-   
+
 </section>
